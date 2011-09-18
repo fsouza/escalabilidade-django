6ffbcc8b714c71d10bc6a91d29654bdf2fb4d324
Francisco Souza <f@souza.cc>
2011-09-18 03:00:07 -0300

    Changed target name

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ from fabric.api import local
 def clean():
     local('git clean -dfX')
 
-def generate():
+def build():
     clean()
     local('landslide escalabilidade.cfg')
     local('open escalabilidade.html')
