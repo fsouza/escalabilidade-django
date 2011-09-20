2d7b3c6873ae746d8ecc287bfc71adda8356fddd
Francisco Souza <f@souza.cc>
2011-09-19 23:45:30 -0300

    Compressing for deployment

--- escalabilidade_django/fabfile.py
+++ escalabilidade_django/fabfile.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from fabric.api import cd, env, local, put, roles, run
+from fabric.api import cd, lcd, env, local, put, roles, run
 
 env.user = 'xikin'
 env.presentation_name = 'escalabilidade-django'
 env.url = 'http://p.souza.cc/%(presentation_name)s' % env
 env.presentations_directory = '/home/xikin/p.souza.cc'
+env.tmp_dir = '/tmp/escalabilidade'
 env.remote_directory = '%(presentations_directory)s/%(presentation_name)s' % env
 env.roledefs = {
     'remote' : ['p.souza.cc',],
@@ -20,9 +21,24 @@ def build():
 def open():
     local('open index.html')
 
+def compress():
+    local('mkdir -p %s' % env.tmp_dir)
+    local('cp -r * %s' % env.tmp_dir)
+    with lcd(env.tmp_dir):
+        local('pyminify index.html > index_minified.html')
+        local('mv index_minified.html index.html')
+        local('java -jar yuicompressor.jar theme/css/screen.css > theme/css/screen-minified.css')
+        local('java -jar yuicompressor.jar theme/css/print.css > theme/css/print-minified.css')
+        local('java -jar yuicompressor.jar theme/js/slides.js > theme/js/slides-minified.js')
+        local('sed -i old "s/screen\.css/screen-minified.css/g" index.html')
+        local('sed -i old "s/print\.css/print-minified.css/g" index.html')
+        local('sed -i old "s/slides\.js/slides-minified.js/g" index.html')
+
 def package():
     build()
-    local('tar -czvf /tmp/escalabilidade.tar.gz img index.html theme')
+    compress()
+    with lcd('/tmp/escalabilidade'):
+        local('tar -czvf /tmp/escalabilidade.tar.gz img index.html theme')
 
 @roles('remote')
 def deploy():
