8ee5a92b24af3b1e3b08cfbb679469bb7b2afa11
Francisco Souza <f@souza.cc>
2011-09-25 22:33:29 -0300

    Template directory

--- escalabilidade_django/samples/library/settings.py
+++ escalabilidade_django/samples/library/settings.py
@@ -1,5 +1,9 @@
 # Django settings for library project.
 
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -106,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
