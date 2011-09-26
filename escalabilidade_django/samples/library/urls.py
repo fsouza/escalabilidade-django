a57cabb0e1f089dcc9f95e107716de4dc9c31dc8
Francisco Souza <f@souza.cc>
2011-09-25 21:52:40 -0300

    Fixed include

--- escalabilidade_django/samples/library/urls.py
+++ escalabilidade_django/samples/library/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    include('books.urls', namespace='books', app_name='books'),
+    url('^books', include('books.urls', namespace='books', app_name='books')),
 )
