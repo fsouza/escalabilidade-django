51e824d7c05e411f7bbfaa6f56bba5700ef08cbc
Francisco Souza <f@souza.cc>
2011-09-26 22:41:50 -0300

    Better code for the middleware

--- samples/library/middleware.py
+++ samples/library/middleware.py
@@ -9,16 +9,18 @@ from django.conf import settings
 
 class ProfileMiddleware(object):
 
+    prof = None
+
     def process_request(self, request):
         if settings.DEBUG and 'prof' in request.GET:
             self.prof = cProfile.Profile()
 
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        if settings.DEBUG and 'prof' in request.GET:
+        if self.prof:
             return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
 
     def process_response(self, request, response):
-        if settings.DEBUG and 'prof' in request.GET:
+        if self.prof:
             self.prof.disable()
             prof_out = StringIO()
             old_stdout = sys.stdout
