4195aea6b39f5b8759f1e74f235053461760a5f6
Francisco Souza <f@souza.cc>
2011-09-26 22:27:36 -0300

    Sample of isolated profiling

---
+++ samples/library/profile.py
@@ -0,0 +1,13 @@
+import cProfile
+
+from django.test.client import RequestFactory
+
+from books.views import list_books
+
+factory = RequestFactory()
+
+request = factory.get('/books')
+profile = cProfile.Profile()
+profile.runcall(list_books, request)
+import pdb; pdb.set_trace()
+profile.print_stats()
