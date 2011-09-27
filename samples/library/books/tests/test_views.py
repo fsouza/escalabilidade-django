110f1102972dffc7eed99050f993b4d8742407d7
Francisco Souza <f@souza.cc>
2011-09-26 21:52:18 -0300

    Dummy test for books loading

--- samples/library/books/tests/test_views.py
+++ samples/library/books/tests/test_views.py
@@ -2,6 +2,8 @@
 from django.test import TestCase
 from django.test.client import Client, RequestFactory
 
+from books.views import list_books
+
 
 class ViewsTestCase(TestCase):
     fixtures = ['books.yaml']
@@ -13,3 +15,10 @@ class ViewsTestCase(TestCase):
     def test_should_exists_a_url_for_books(self):
         response = self.client.get('/books')
         self.assertEquals(200, response.status_code)
+
+    def test_should_return_the_books_list_in_the_context(self):
+        request = self.factory.get('/books')
+        response = list_books(request)
+        books_list = response.context_data['books']
+
+        self.assertEquals(23, len(books_list))
--- samples/library/books/views.py
+++ samples/library/books/views.py
@@ -1,5 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+from books.models import Book
+
 
 def list_books(request):
-    return HttpResponse("Ok")
+    books = Book.objects.all()
+    return TemplateResponse(request, "books_list.html", locals())
